2ade8fbd3327b8037a9fec80f2daa117b03bec02
Pratik Chavan <chavanpratik.pc@gmail.com>
2023-01-16 12:26:57 -0500

    fixing bugs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,28 @@
-import { useState } from 'react';
-import Navigation from './Navigation';
-import Hamburger from '../assets/hamburger.png';
-import Close from '../assets/close.png';
+import { useState } from "react";
+import Navigation from "./Navigation";
+import Hamburger from "../assets/hamburger.png";
+import Close from "../assets/close.png";
 export default function Navbar() {
-    const [navbarOpen, setNavbarOpen] = useState(false);
-    function handleToggle() {
-        setNavbarOpen(!navbarOpen)
-    }
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  function handleToggle() {
+    setNavbarOpen(!navbarOpen);
+  }
 
-    return (
-        <nav>
-            <nav className="burger">
+  return (
+    <nav>
+      <nav className="burger">
+        <img
+          src={require("../assets/nav-logo.png")}
+          alt="Little Lemon logo"
+          className="nav-image"
+        ></img>
 
-                <img src={require('../assets/nav-logo.png')}
-                alt="Little Lemon logo"
-                className="nav-image">
-                </img>
-
-                <button className="burger-icon" onClick = {handleToggle}>
-                    <img src={navbarOpen ? Close : Hamburger} alt="Navigation Bar"/>
-                </button>
-
-            </nav>
-            <Navigation device="desktop" />
-            {navbarOpen ? <Navigation device="mobile" /> : ""}
-        </nav>
-    );
+        <button className="burger-icon" onClick={handleToggle}>
+          <img src={navbarOpen ? Close : Hamburger} alt="Navigation Bar" />
+        </button>
+      </nav>
+      <Navigation device="desktop" />
+      {navbarOpen ? <Navigation device="mobile" /> : ""}
+    </nav>
+  );
 }
--- src/components/sections/headingPages/CardInfo/SpecialCard.js
+++ src/components/sections/headingPages/CardInfo/SpecialCard.js
@@ -1,14 +1,20 @@
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 export default function SpecialCard(props) {
-    return (
-        <article className="menu-card">
-            <img src={props.image} alt="Special Menu"></img>
-            <section className="menu-card-content">
-                <h1>{props.name}</h1>
-                <h3>{props.price}</h3>
-                <p>{props.description}</p>
-                <Link className="special-button"to="/order">Order for Delivery</Link>
-            </section>
-        </article>
-    );
+  return (
+    <article className="menu-card">
+      <img
+        src={props.image}
+        style={{ borderRadius: "10px 10px 0 0" }}
+        alt="Special Menu"
+      ></img>
+      <section className="menu-card-content">
+        <h1>{props.name}</h1>
+        <h3>{props.price}</h3>
+        <p style={{ margin: "15px 0 38px 0" }}>{props.description}</p>
+        <Link className="special-button" to="/order">
+          Add to cart
+        </Link>
+      </section>
+    </article>
+  );
 }
